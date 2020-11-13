182c759cac25ddf94150d2ef9dd0630b7f24c93b
MACNIACOS <macniacos@MACNIACOSs-iMac-2.local>
2020-11-13 13:40:01 +0100

    templates pages

--- application/views/home.php
+++ application/views/home.php
@@ -1,17 +1,1 @@
-<html>
-<head>
-<title>Portada de Codeigniter</title>
-</head>
-<body>
-<h1>CodeIgniter test</h1>
-<p>La portada de página web, basada en codeigniter</p>
-<p>Estos son los últimos artículos publicados.</p>
-<?php
-while ($fila = mysql_fetch_array($rs_articulos)){
-   echo '<p>';
-   echo '<a href="' . site_url('/articulos/muestra' . $fila['id']) . '">' . $fila['titulo'] . '</a>';
-   echo '</p>';
-}
-?>
-</body>
-</html>
+This is home
